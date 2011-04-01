dc79fe8a4944ffc47a154cfa5d98695b32b6b02b
shawn.button <shawnbutton@gmail.com>
2011-03-31 22:18:23 -0400

    first tet

---
+++ test/end-to-end/com/shawnbutton/auctionsniper/endToEnd/AuctionSniperEndToEndTest.java
@@ -0,0 +1,26 @@
+package com.shawnbutton.auctionsniper.endToEnd;
+
+public class AuctionSniperEndToEndTest {
+    private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+    private final ApplicationRunner application = new ApplicationRunner();
+
+    @Test
+    public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
+        auction.startSellingItem(); // Step 1
+        application.startBiddingIn(auction); // Step 2
+        auction.hasReceivedJoinRequestFromSniper(); // Step 3
+        auction.announceClosed(); // Step 4
+        application.showsSniperHasLostAuction(); // Step 5
+    }
+
+    // Additional cleanup
+    @After
+    public void stopAuction() {
+        auction.stop();
+    }
+
+    @After
+    public void stopApplication() {
+        application.stop();
+    }
+}
