74077670857d303f02942006b81c280ca321c169
shawn.button <shawnbutton@gmail.com>
2011-04-18 11:16:23 -0400

    Added jmock test runner

--- test/unit/test/auctionsniper/AuctionMessageTranslatorTest.java
+++ test/unit/test/auctionsniper/AuctionMessageTranslatorTest.java
@@ -6,8 +6,11 @@ import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.packet.Message;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JMock.class)
 public class AuctionMessageTranslatorTest {
 
     public static final Chat UNUSED_CHAT = null;
